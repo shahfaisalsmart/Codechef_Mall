2b160ce80b00d5e21eea61778140cc540407fe4a
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-08-23 14:43:55 +0530

    Solved Successfully

---
+++ PCJ19A.cpp
@@ -0,0 +1,272 @@
+///==================================================///
+///               !! HELLO WORLD !!                  ///
+///                 >>IT'S ME<<                      ///
+///               MOHAMMAD FAISAL                    ///
+///==================================================///
+//THIS CODE IS IMPLEMENTED BY DUGGU_123(MOHAMMAD FAISAL)
+// author: MOHAMMAD FAISAL (email: mohammad.faisal78612@gmail.com)
+/*
+ 
+        _/ _/ _/ _/  _/ _/ _/ _/   _/  _/ _/ _/   _/ _/ _/ _/  _/
+       _/           _/       _/   _/  _/         _/       _/  _/
+      _/ _/ _/     _/ _/ _/ _/   _/  _/ _/ _/   _/ _/ _/ _/  _/
+     _/           _/       _/   _/        _/   _/       _/  _/
+    _/           _/       _/   _/  _/ _/ _/   _/       _/  _/ _/ _/ _/
+ 
+ */
+/*
+ ``````````````````````¶¶¶
+ ```````````````````¶¶¶¶¶
+ ``````````````````¶¶¶¶¶¶
+ ````````````````¶¶¶¶¶¶¶
+ ```````````````¶¶¶¶¶¶¶¶
+ ```````````````¶¶¶¶¶¶¶¶
+ ``````````````¶¶¶¶¶¶¶¶¶¶
+ ``````````````¶¶¶¶¶¶¶¶¶¶
+ ``````````````¶¶¶¶¶¶¶¶¶¶¶``````````````¶¶¶
+ ``````````````¶¶¶¶¶¶¶¶¶¶¶¶```````````¶¶¶¶
+ ```````¶``````¶¶¶¶¶¶¶¶¶¶¶¶¶````````¶¶¶¶¶¶
+ ```````¶¶¶¶````¶¶¶¶¶¶¶¶¶¶¶¶¶``````¶¶¶¶¶¶¶
+ ```````¶¶¶¶¶```¶¶¶¶¶¶¶¶¶¶¶¶¶¶````¶¶¶¶¶¶¶¶
+ ```````¶¶¶¶¶¶```¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶``¶¶¶¶¶¶¶¶
+ ```````¶¶¶¶¶¶¶``¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ ```````¶¶¶¶¶¶¶``¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ ``````¶¶¶¶¶¶¶¶``¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ `````¶¶¶¶¶¶¶¶¶`¶¶¶¶¶¶¶¶¶¶``¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ ```¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶```¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ ``¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶```¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ ``¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶````¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ `¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶`¶¶¶¶¶¶````¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ `¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶``¶¶¶``````¶¶¶`¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ `¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶```¶````````¶¶``¶¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ `¶¶¶¶¶¶¶¶¶¶¶¶¶¶`````````````¶¶``¶¶¶¶¶¶¶¶¶¶¶¶¶¶
+ `¶¶¶¶¶¶¶¶¶¶¶¶¶¶``````````````¶````¶¶¶¶¶¶¶¶¶¶¶
+ ``¶¶¶¶¶¶¶¶¶¶¶¶`````````````````````¶¶¶¶¶¶¶¶¶
+ ````¶¶¶¶¶¶¶¶¶¶`````````````````````¶¶¶¶¶¶¶¶
+ ``````¶¶¶¶¶¶¶¶`````````````````````¶¶¶¶¶¶
+ `````````¶¶¶¶¶¶```````````````````¶¶¶¶
+ `````````````¶¶¶¶¶``````````````¶
+ */
+ // #include<bits/stdc++.h>
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<set>
+#include<math.h>
+using namespace std ;
+#define test() int t; cin >> t; while(t--)
+#define ll long long
+#define ull unsigned long long int
+#define ld long double
+#define lop(i,n) for(int i=0;i<n;i++)
+#define rloop(i,n) for (int i = n-1; i >= 0; i--)
+#define loop(x, n) for(ll x = 0; x < n; ++ x)
+#define rep(x,n) for(ll x=1;x<=n;x++)
+#define mp make_pair
+#define pb push_back
+#define I cin>>
+#define O cout<<
+#define W(x) while(x--)
+#define si(x) scanf("%d",&x)
+#define pi(x) printf("%d\n",x)
+#define s(x) scanf("%lld",&x)
+#define p(x) printf("%lld\n",x)
+#define sc(x) scanf("%s",x)
+#define pc(x) printf("%s",x)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define out2(x, y) cout<< (x) << " " << (y) << endl
+#define out3(x, y, z) cout<< (x) << " " << (y) << " " << (z) << endl
+#define F first
+#define S second
+#define inf 1e18
+#define MAX 10000000
+#define M 1000000007
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) ((a)*(b))/gcd((a),(b))
+#define prec(x) fixed<<setprecision(15)<<x
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define magic ios_base::sync_with_stdio(false); cin.tie(0);cout.tie(0)
+#define XXX return 0;
+#define br cout<<endl;
+#define RESET(a,b)	memset(a,b,sizeof(a))
+#define READ(f) freopen(f, "r", stdin)
+#define WRITE(f) freopen(f, "w", stdout)
+#define _abs(x, y) ((x>y)?(x-y):(y-x))
+const double PI=3.141592653589793238462643383279502884197169399375105820974944;
+//const ll inf = 1000000000;
+//const ll MOD = 1000000007;
+vector<bool> chkprime (MAX,true);
+unsigned primearray[5761459];
+// Inline functions
+// inline bool EQ(double a, double b) { return fabs(a-b) < 1e-9; }
+// inline bool isLeapYear(ll year) { return (year%400==0) || (year%4==0 && year%100!=0); }
+// inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+// inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+// inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+// inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+// inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+// inline ll modInverse(ll a) { return modPow(a, MOD-2); }
+// inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+// inline bool isInside(pii p,ll n,ll m)	{ return (p.first>=0&&p.first<n&&p.second>=0&&p.second<m); }
+// inline bool isInside(pii p,ll n)		{ return (p.first>=0&&p.first<n&&p.second>=0&&p.second<n); }
+// inline bool isSquare(ll x) 				{ ll s = sqrt(x);	return (s*s==x); }
+// inline bool isFib(ll x) 				{ return isSquare(5*x*x+4)|| isSquare(5*x*x-4); }
+// inline bool isPowerOfTwo(ll x)			{ return ((1LL<<(ll)log2(x))==x); }
+
+
+// //Bit Operations
+// inline bool checkBit(ll n, int i) { return n&(1LL<<i); }
+// inline ll setBit(ll n, int i) { return n|(1LL<<i);; }
+// inline ll resetBit(ll n, int i) { return n&(~(1LL<<i)); }
+
+// void n_primes(){
+//     chkprime[0]=chkprime[1]=false;
+//     for(unsigned i=2;i*i<=MAX;i++){
+//         if(chkprime[i]){
+//             for(unsigned j=i;j*i<=MAX;j++) chkprime[i*j]=false;
+//         }
+//     }
+//     unsigned id = 1;
+//     primearray[0]=2;
+    
+//     for(int i=3;i<=MAX;i+=2){
+//         if(chkprime[i]){
+//             primearray[id++]=i;
+//         }
+//     }
+// }
+
+// bool primity(long long n)
+// {
+//     if(n<=1)
+//         return false;
+//     if(n<=3)
+//         return true;
+    
+//     if(n%2==0 || n%3==0)
+//         return false;
+    
+//     for(auto i=5; i*i<=n; i+=6)
+//         if(n%i==0 || n%(i+2)==0)
+//             return false;
+    
+//     return true;
+// }
+
+//  ───────────▄▄▄▄▄▄▄▄▄▄▄▄▄
+//  ────────▄▀▀─────────────▀▀▄
+//  ───────█───────────────────█
+//  ──────█─────────────────────█
+//  ─────█───▄▄▄▄▄▄▄───▄▄▄▄▄▄▄───█
+//  ────█───█████████─█████████───█
+//  ────█──██▀────▀█████▀────▀██──█
+//  ───██████───█▀█─███───█▀█─██████
+//  ───██████───▀▀▀─███───▀▀▀─██████
+//  ────█──▀█▄────▄██─██▄────▄█▀──█
+//  ────█────▀█████▀───▀█████▀────█
+//  ────█───────────────▄▄▄───────█
+//  ────█───────▄▄▄▄██▀▀█▀▀█▄─────█
+//  ────█─────▄██▄█▄▄█▄▄█▄▄██▄────█
+//  ────█─────▀▀█████████████▀────█
+//  ───▐▓▓▌─────────────────────▐▓▓▌
+//  ───▐▐▓▓▌▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▐▓▓▌▌
+//  ───█══▐▓▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄▓▌══█
+//  ──█══▌═▐▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▌═▐══█
+//  ──█══█═▐▓▓▓▓▓▓▄▄▄▄▄▄▄▓▓▓▓▓▓▌═█══█
+//  ──█══█═▐▓▓▓▓▓▓▐██▀██▌▓▓▓▓▓▓▌═█══█
+//  ──█══█═▐▓▓▓▓▓▓▓▀▀▀▀▀▓▓▓▓▓▓▓▌═█══█
+ 
+//  ──█───█─█──█─█▀▀█─▀▀█▀▀─▀█─█─▀█──
+//  ──█▄█▄█─█▀▀█─█▄▄█───█───█▀─▀─█▀──
+//  ───▀─▀──▀──▀─▀──▀───▀───▄──▄─▄───
+ 
+// int power_x(int x, unsigned y)
+// {
+//     if(y==0)
+//         return 1;
+//     if(!(y&1))
+//         return power_x(x,y>>1)*power_x(x,y>>1);
+//     return x*power_x(x,y>>1)*power_x(x,y>>1);
+// }
+// int sod(int n)
+// {
+// 	int sum=0;
+// 	while(n > 0)
+// 	{
+// 		sum+=n%10;
+// 		n/=10;
+// 	}
+// 	return sum;
+// }
+// int order_x(long long x)
+// {
+//     int n=0;
+//     while(x)
+//     {
+//         n++;
+//         x/=10;
+//     }
+//     return n;
+// }
+// bool is_perfect_square(int n) {
+//     if (n < 0)
+//         return false;
+//     int root(round(sqrt(n)));
+//     return n == root * root;
+// }
+
+// bool is_perfect_cube(int n) {
+//     int root(round(cbrt(n)));
+//     return n == root * root * root;
+// }
+
+/*bool isArm(long long n)
+ {
+ int pow=order_x(n);
+ long long temp=n,sum=0;
+ while(temp)
+ {
+ int r=temp%10;
+ sum+=power_x(r,pow);
+ temp/=10;
+ }
+ if(sum==n)
+ return true;
+ else
+ return false;
+ }*/
+// bool compare(v.first, c.first)
+// {
+// 	return v.first < c.first;
+// }
+int main ()
+{
+    magic;
+    /*
+     
+     O
+    <C>
+     /\
+     
+     */
+		string s;
+		I s;
+		bool flag=false;
+		int len = (int)s.size();
+		lop(i,len){
+			if(s[i]=='x')
+			{
+				O "Not Safe";
+				flag=true;
+				break;
+			}
+		}
+		if(flag==false)
+			O "Safe";
+
+    XXX
+}
