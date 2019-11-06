ccf461f1324169460ffd9ba6ecb5e646a1842fc5
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-11-06 18:02:41 +0530

    new file added

---
+++ ZCOSCH.cpp
@@ -0,0 +1,17 @@
+// BEGINNING WITH THE NAME OF ALMIGHTY GOD ALLAH
+// AUTHOR:: MOHAMMAD FAISAL
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ 
+ long long n;
+ cin >> n;
+ if(n>=1 && n<=50)
+ 	cout<<"100"<<endl;
+ else if(n>50 && n<=100)
+ 	cout<<"50"<<endl;
+ else 
+ 	cout<<"0"<<endl;
+ return 0;
+ }
